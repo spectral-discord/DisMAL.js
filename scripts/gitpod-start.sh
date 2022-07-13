76e50a383f03305b1e76a7ad4c059104d4529e22
Lina <git@lina.pizza>
2022-07-13 05:23:09 +0000

    package bits

--- scripts/gitpod-start.sh
+++ scripts/gitpod-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ ! -z "${NPM_TOKEN}" ] && [ ! -f "/workspace/DisMAL.js/.npmrc" ]; then
-  printf "//npm.pkg.github.com/:_authToken=${NPM_TOKEN}\n@spectral-discord:registry=https://npm.pkg.github.com" >> /workspace/DisMAL.js/.npmrc
+  printf "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> /workspace/DisMAL.js/.npmrc
 fi
